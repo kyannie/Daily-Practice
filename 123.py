955d0084990de0abe3d8c78bc256eea0c174bc2f
kyannie <46258234+kyannie@users.noreply.github.com>
2019-02-12 13:37:48 +0000

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,19 @@
+x = 1
+print(x)
+print(type(x))
+
+y = "1"
+print(type(y))
+
+z = 1.61
+print(type(z))
+
+a = "I"
+b = " am"
+print(a + b) #Comment
+
+c = 5  #Auto define data type as Integer
+c = int("5")
+#c = str(5)  #Define data type as String manually
+d = " is five"
+print(c + d)
