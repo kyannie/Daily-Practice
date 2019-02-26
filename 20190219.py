e6899a43471d71eabf8d67ec883cb533b98dcd82
kyannie <46258234+kyannie@users.noreply.github.com>
2019-02-26 14:25:31 +0000

    Add files via upload

--- 20190219.py
+++ 20190219.py
@@ -21,13 +21,12 @@ print("a\ta")
 print("a\na")
 
 b = "Hello World"
-c = b.replace(" ", "*")
+b = b.replace(" ", "*")
 print(b.replace("l", "i"))
 
 print(b.split(" "))
 
 d = "I am a mouse ."
-print(d.split("a"))
 
 e = ["a", 1, "c"]
 print(e)
@@ -42,3 +41,77 @@ e[2] = "d"
 print(e)
 
 print(len(e))
+
+x=["a","b","c"]
+x.append("d")
+print(x)
+x.pop(1)
+print(x)
+x.append("1")
+print(x)
+
+print(2==1+1)
+
+print(2 > 1)
+print(3 >= 2)
+
+print(-2 < 2)
+print(-2 <= 2)
+
+print(1 > 2)
+print(2 <= 2)
+
+a = 2
+b = 3
+if a == b:
+    print("Hi this is True")
+else:
+    print("Bye this is false")
+    print("Bye Bye")
+    
+print("Hi")
+
+a = 1
+b = 2
+if a == b:
+    print("This is true")
+elif a > b:
+    print("A is greater than B")
+else:
+    print("This is False")
+
+mum = 10
+while mum > 0 :
+    mum = mum - 1
+print("Loop End")
+
+family = ["cat", "dog", "mouse"]
+for x in family:
+    print(x)
+print("For Loop End")
+
+numbers = [0, 1, 2, 3, 4, 5]
+for i in numbers:
+    print(i)
+print("For Loop End")
+
+x = [1,2,3,4,5,6,7,8,9]
+for i in x:
+    if i%2 == 1:
+        print("Odd Number")
+    else:
+        print("Even Number")
+print("Loop 1 End")
+
+y = 30
+while y > 0:
+    y = y-1
+    if y%3 == 0:
+        print("Yeah")
+    elif y%5 == 0:
+        print("Ohhh")
+    else:
+        print("Sad")
+print("Loop 2 End")
+
+
